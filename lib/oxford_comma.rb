db863315b50dbdba370756a42705bcaad59e3433
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-09 23:56:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,7 @@ def oxford_comma(array)
   else if array.lenth == 2
     array.join("and")
   else if array.length >= 3
-    array[-1] = "and array[-1]"
+    array[-1] = "and #{array[-1]}"
+end
+end
 end
