e736694ccc962b09dd30209084f6e0a1d59108a5
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-09 23:58:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
   if array.length == 1
     array.join("")
   else if array.length == 2
-    array.join("and")
+    array.join(" and ")
   else if array.length >= 3
     array[-1] = "and #{array[-1]}"
-    array.join(",")
+    array.join(", ")
   else
     nil
 end
